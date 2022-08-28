81a24832ddb61258d9697bedf203c30c395a9cd3
user.name <user.email>
2022-08-28 23:49:45 +0900

    baekjoon 괄호 문제풀이
     Author:    user.name <user.email>
     Date:      Sun Aug 28 23:49:45 2022 +0900
    
     On branch main
     Changes to be committed:
    	new file:   "\352\264\204\355\230\270.cpp"
    	new file:   "\354\206\214\354\210\230\352\265\254\355\225\230\352\270\260.cpp"

---
+++ 괄호.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    int n;
+    string arr;
+    cin >> n;
+    cin.ignore();
+    for(int i = 0;i < n;i++){
+        int flag = 0;
+        stack<int> stk;
+        cin >> arr;
+        for(int j = 0;arr[j] != '\0';j++){
+            if(arr[j] == '(')stk.push(1);
+            else {
+                if(stk.empty()) {
+                    cout << "NO" << '\n';
+                    flag = 1;
+                    break;
+                } 
+                else stk.pop();
+            }
+        }
+				if(!stk.empty()) cout << "NO" << '\n';
+        else if(flag == 0) cout << "YES" << '\n';
+    }
+    return 0;
+}
