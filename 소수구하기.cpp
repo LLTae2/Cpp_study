24c571522a833c776423e8e061be6b02573771b7
LLTae2 <taehyun5820@gmail.com>
2022-08-28 23:49:45 +0900

    baekjoon 괄호 문제풀이
     Author:    user.name <user.email>
     Date:      Sun Aug 28 23:49:45 2022 +0900
    
     On branch master
     Changes to be committed:
    	new file:   "\352\264\204\355\230\270.cpp"
    	new file:   "\354\206\214\354\210\230\352\265\254\355\225\230\352\270\260.cpp"

---
+++ 소수구하기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m, flag = 1;
+    cin >> n >> m;
+    for(int i = n;i <= m;i++){
+        
+        
+    }
+
+    return 0;
+}
